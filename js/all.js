82eb6492bd9feececf125a119733ebdb744e28f5
EL KADI MUSTAPHA <mustaphaettanti@gmail.com>
2021-10-25 14:18:33 +0100

    commit v79

--- js/all.js
+++ js/all.js
@@ -107,3 +107,18 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+function myFunction2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "اقرأ المزيد"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "إخفاء"; 
+    moreText.style.display = "inline";
+  }
+}
