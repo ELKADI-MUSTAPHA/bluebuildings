d55c52d5d9378401f5c55362f5f79c8bc34cdd4e
EL KADI MUSTAPHA <mustaphaettanti@gmail.com>
2021-11-08 15:36:21 +0100

    commit v87

--- js/main.js
+++ js/main.js
@@ -49,22 +49,19 @@ const sr5 = ScrollReveal({
   reset: false,
 });
 
-sr5.reveal(`.offering .col`, {
+sr5.reveal(`.offering .col, .why-us`, {
   interval: 150,
 });
-// if (window.innerWidth <= 700) {
-//   window.setTimeout(function () {
-//     const sr5 = ScrollReveal({
-//       origin: "bottom",
-//       distance: "200px",
-//       duration: 1000,
-//       reset: false,
-//     });
-//     sr5.reveal(`.span-wts`, {
-//       interval: 150,
-//     });
-//   }, 3000);
-// }
+const sr7 = ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+  reset: false,
+});
+
+sr7.reveal(`.row a`, {
+  interval: 150,
+});
 
 if (window.innerWidth < 600) {
   sr5.reveal(`.img-one, .img-two, .h2.one, .h2.two`, {
